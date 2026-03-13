32d04147911d442b0ecfec75e64de0104b7886d5
Scalabrini17 <itallomartins2007@gmail.com>
2026-03-13 19:30:25 -0300

    Adicionado mais unidades e .gitignore para anotações

---
+++ Nivel 2/Unidade_4_2.java
@@ -0,0 +1,19 @@
+//4. Calcular o valor total de uma compra (preço × quantidade).
+import java.util.Scanner;
+
+public class Unidade_4_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de cada item comprado: ");
+        int quantiItens = sc.nextInt();
+
+        System.out.print("Digite o valor do item comprado: ");
+        float valorItem = sc.nextFloat();
+
+        double valorFinal = valorItem * quantiItens;
+
+        System.out.println("O valor total da sua compra é: " + valorFinal);
+
+    }
+}
