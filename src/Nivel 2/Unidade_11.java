4591205963850dfefc9f1dc4bd10efe4b14d1020
Scalabrini17 <itallomartins2007@gmail.com>
2026-04-01 17:01:11 -0300

    exercise updade

--- src/Nivel 2/Unidade_11.java
+++ src/Nivel 2/Unidade_11.java
@@ -4,7 +4,27 @@ public class Unidade_11 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println(" Taxi Fatesg");
+        System.out.println("---------------");
+
+        System.out.print("Digite o nome do Passageiro: ");
+        String nomepass = sc.next();
+
+        System.out.print("Digite qual o valor por quilometro rodado: ");
+        double valorporkm = sc.nextDouble();
+
+        System.out.print("Digite quantos quilometros foram rodados: ");
+        double kmrodado = sc.nextDouble();
+
+        System.out.print("Por fim digite o valor fixo da corrida: ");
+        double valorfix = sc.nextDouble();
+
+        // Calculos!
+        double valorKmTotal = kmrodado * valorporkm;
+        double valorFinal = valorKmTotal + valorfix;
+
+        // Resposta final!
+        System.out.printf("A corrida do passageiro %s é %.2f", nomepass, valorFinal );
 
     }
 }
